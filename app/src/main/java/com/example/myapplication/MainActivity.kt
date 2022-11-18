b757fa566a5486f5ceed242b0068fd72f2db6d15
luq18 <luqman.hrs@eduge.ch>
2022-11-18 10:12:54 +0100

    début de android studio

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,11 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.random.Random
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +15,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton :Button = findViewById(R.id.roll_button)//créer une valeur en l'associant à l'id créer dans le fichier activity_main (android:id="@+id/roll_button")
         rollButton.text="Let's Roll"//Prendre le boutton en écrivant du text
+        rollButton.setOnClickListener{rollDice()}
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        val randomInt = java.util.Random().nextInt(6) +1
+        resultText.text=randomInt.toString()
     }
 }
