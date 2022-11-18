2839be30170f50c56f27bd696d5e6e5f5c435795
luq18 <luqman.hrs@eduge.ch>
2022-11-18 11:26:46 +0100

    j'ai fini la partie concept

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -11,13 +11,18 @@ import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton :Button = findViewById(R.id.roll_button)//créer une valeur en l'associant à l'id créer dans le fichier activity_main (android:id="@+id/roll_button")
         rollButton.text="Let's Roll"//Prendre le boutton en écrivant du text
         rollButton.setOnClickListener{rollDice()}
+        diceImage=findViewById(R.id.dice_image)
     }
+
+
+
 
     private fun rollDice() {
 
@@ -32,7 +37,7 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
 
-        val diceImage :ImageView = findViewById(R.id.dice_image)
+
         diceImage.setImageResource(drawableResource)
 
     }
